9ed2eb602414d6a116c3d3a87790d00703338608
Brylle <luckybrylleramos@gmail.com>
2025-10-23 11:25:05 +0400

    Hide the section on leader-caravans, supreme-caravans and goldstream-rv

--- custom-blocks/content-arch-block.php
+++ custom-blocks/content-arch-block.php
@@ -1,6 +1,11 @@
 <?php
 $post_slug = get_post_field( 'post_name', get_post() );
 ?>
+<style>
+    section.curvy-content-wrapper.leader-caravans, section.curvy-content-wrapper.supreme-caravans, section.curvy-content-wrapper.goldstream-rv {
+        display: none;
+    }
+</style>
 <section class="curvy-content-wrapper content-arch <?php the_field('classes') ?> <?php echo esc_attr( $post_slug ); ?>">
     <div class="content-arch-container curvy-container container">
         <div class="container" data-aos="fade-up">
