429c571d031b90127472ed4aedfda4dca69989c3
lovelyramos <lovely@simplycro.com.au>
2025-11-13 14:07:44 +0400

    Forms

--- custom-blocks/newsletter-subscription-form-block.php
+++ custom-blocks/newsletter-subscription-form-block.php
@@ -4,8 +4,8 @@
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="intro">
-                <h2>“Adventure is out there.”</h2>
-                <p>- Charles Muntz</p>
+                    <h2>“Adventure is out there.”</h2>
+                    <p>- Charles Muntz</p>
                 </div>
             </div><!-- end col -->
         </div><!-- end row -->
@@ -15,10 +15,13 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="form-intro">
-                    <p>Stay connected with Adelaide RV by signing up for our new fortnightly newsletter! It's your direct line to 
-                    the latest hints and tips, exclusive updates on events like our annual Family and Friends Weekend, and inspiring 
-                    getaway ideas from Haydo. Don't miss out on the adventure—join our community today and start planning your next 
-                    escape with confidence and insider knowledge.</p>
+                    <p>Stay connected with Adelaide RV by signing up for our new fortnightly newsletter! It's your
+                        direct line to
+                        the latest hints and tips, exclusive updates on events like our annual Family and Friends
+                        Weekend, and inspiring
+                        getaway ideas from Haydo. Don't miss out on the adventure—join our community today and start
+                        planning your next
+                        escape with confidence and insider knowledge.</p>
                 </div>
             </div><!-- end col -->
         </div><!-- end row -->
@@ -28,12 +31,80 @@
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="form-wrapper">
-                        <?php echo do_shortcode('[wpforms id="1136" title="false"]');?>
+                    <script src="https://js-ap1.hsforms.net/forms/embed/developer/50319163.js" defer></script>
+                    <div class="hs-form-html" data-region="ap1" data-form-id="0577d27c-205c-48fa-8075-4ca05517daed"
+                         data-portal-id="50319163"></div>
                 </div>
             </div><!-- end col -->
         </div><!-- end row -->
     </div><!-- end container -->
 
 </section>
+<style>
+    #subscribe [data-hsfc-id=Renderer] .hsfc-Step {
+        background: none !important;
+    }
 
+    #subscribe [data-hsfc-id=Renderer] .hsfc-Heading {
+        display: none;
+    }
 
+    #subscribe .hsfc-Row {
+        margin: 0;
+    }
+
+    #subscribe .hsfc-RichText {
+        margin-bottom: 30px;
+    }
+
+    #subscribe [data-hsfc-id=DataPrivacyField] .hsfc-RichText {
+        margin: 0;
+        font-size: 14px;
+    }
+
+    #subscribe [data-hsfc-id=FieldLabel] {
+        float: none;
+        line-height: 1.3;
+        margin: 10px 0 4px 0;
+        padding: 0;
+        word-break: break-word;
+        word-wrap: break-word;
+        margin-left: 20px !important;
+        font-weight: 400 !important;
+        font-size: 1.7rem !important;
+        color: white;
+    }
+
+    #subscribe [data-hsfc-id=Renderer] .hsfc-ErrorAlert {
+        font-size: 12px;
+        float: none;
+        cursor: default;
+        margin-top: 0;
+    }
+
+    #subscribe [data-hsfc-id=Renderer] .hsfc-Step .hsfc-Step__Content {
+        padding: 0;
+    }
+
+    #subscribe [data-hsfc-id=Renderer] .hsfc-TextInput, [data-hsfc-id=Renderer] .hsfc-TextareaInput {
+        font-size: 16px;
+        font-family: inherit;
+        border: 1px solid #ccc;
+        width: 100%;
+        line-height: 1.3;
+        padding-block: 15px;
+    }
+
+    #subscribe [data-hsfc-id=Renderer] .hsfc-PhoneInput__FlagAndCaret {
+        border: 1px solid #ccc;
+    }
+    #subscribe [data-hsfc-id=Renderer] .hsfc-Button {
+        border-radius: 10px;
+        font-size: 18px;
+        font-family: inherit;
+        font-weight: 100;
+    }
+    #subscribe [data-hsfc-id=Renderer] .hsfc-Button:not([disabled]):hover {
+         color: #000 !important;
+         background-color: #fff !important;
+     }</style>
